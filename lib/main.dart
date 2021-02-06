5f55c7f7162cd6855a77266ce1a3c20b9266ea5d
Simon Hoyos Cadavid <45491683+hoyoscadavidsimon@users.noreply.github.com>
2021-02-06 15:59:32 +0100

    Add implementation of Kalman Filter to the main view

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:linalg/linalg.dart';
 import 'package:pc_hauptprojekt/location_detection/IMU.dart';
-import 'package:sensors/sensors.dart';
+import 'package:pc_hauptprojekt/location_detection/kalman_filter.dart';
+import 'package:pc_hauptprojekt/location_detection/lat_lng.dart';
+import 'package:pc_hauptprojekt/utils/distance_calculator.dart';
 
 import 'location_detection/gps.dart';
 
@@ -32,37 +35,73 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   final imu = IMU();
   final gps = Gps();
-  String text = "";
 
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration(milliseconds: 500), () {
-      imu.initializeImu();
-      Timer.periodic(Duration(seconds: 1), (timer) async {
-        final position = await gps.determinePosition();
-      });
-    });
-  }
+  KalmanFilter kalmanFilter;
+
+  /// Reference Vector for the Position in LatLng
+  LatLng positionReference;
+  Vector currentGpsPosition;
+  Vector currentAcceleration;
+  double currentGpsAccuracy;
+
+  int timeOffset = 0;
 
   @override
   Widget build(BuildContext context) {
-    imu.globalAcceleration.addListener(() {
-      text = "East " +
-          imu.globalAcceleration.value[0].toString() +
-          "\n\n" +
-          "North " +
-          imu.globalAcceleration.value[1].toString();
-      setState(() {});
-    });
-
     return Scaffold(
       body: Center(
         child: RaisedButton(
-          onPressed: () {},
-          child: Text(text),
+          onPressed: _onStart,
+          child: Text("Start"),
         ),
       ),
     );
+  }
+
+  void _onStart() async {
+    // Read Initial position
+    final initialPosition = await gps.determinePosition();
+
+    // Set reference values
+    imu.setReferenceSystem();
+    positionReference = LatLng(
+      initialPosition.latitude,
+      initialPosition.longitude,
+    );
+
+    // Initialize Kalman Filter
+    kalmanFilter = KalmanFilter(
+      1 / 100,
+      initialPosition.accuracy,
+    );
+
+    // Perform Reading and filtering every 1/100 seconds.
+    Timer.periodic(Duration(milliseconds: 10), (timer) async {
+      currentAcceleration = imu.getAcceleration();
+
+      // Check the GPS every 1s
+      if (timeOffset >= 100) {
+        // If new dta from GPS, use it.
+        final calculatedPosition = await gps.determinePosition();
+        currentGpsPosition = DistanceCalculator.calculate(
+          positionReference,
+          LatLng(calculatedPosition.latitude, calculatedPosition.longitude),
+        );
+        timeOffset = 0;
+      } else {
+        // If not new Data from GPS, use the predicted location.
+        currentGpsPosition = Vector.column([
+          kalmanFilter.xCorrect[0],
+          kalmanFilter.xCorrect[1],
+        ]);
+        timeOffset++;
+      }
+
+      kalmanFilter.filter(
+        currentGpsPosition,
+        currentAcceleration,
+        currentGpsAccuracy,
+      );
+    });
   }
 }
