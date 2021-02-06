37a0ffd98a426f12aa1773569e2e30af45067687
Simon Hoyos Cadavid <45491683+hoyoscadavidsimon@users.noreply.github.com>
2021-02-06 15:35:13 +0100

    Add LatLng and distance calculator

---
+++ lib/utils/lat_lng_converter.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+
+import 'package:linalg/vector.dart';
+import 'package:pc_hauptprojekt/location_detection/lat_lng.dart';
+
+class DistanceCalculator {
+  /// Calculates the distance in x and y of two Coordinates in degrees
+  static Vector calculate(LatLng start, LatLng end) {
+    final x = 6371000 * cos(end.latitude * pi / 180) * (end.longitude - start.longitude);
+    final y = 111190 * (end.latitude - start.latitude);
+
+    return Vector.column([x, y]);
+  }
+}
