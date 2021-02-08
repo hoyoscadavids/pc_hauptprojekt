002f2f39c71a073e58fa9e80327255a41ada1c3d
Simon Hoyos Cadavid <45491683+hoyoscadavidsimon@users.noreply.github.com>
2021-02-08 17:48:51 +0100

    Add way to save JSON files for the position

---
+++ lib/models/positions.dart
@@ -0,0 +1,30 @@
+class Positions {
+  Positions(this.positions);
+  final List<Pos> positions;
+
+  Positions.fromJson(Map<String, dynamic> json)
+      : positions = json['positions'].map((position) => Pos.fromJson(position));
+
+  Map<String, dynamic> toJson() {
+    return {
+      'positions': positions.map((position) => position.toJson()),
+    };
+  }
+}
+
+class Pos {
+  final double x;
+  final double y;
+
+  Pos(this.x, this.y);
+  Pos.fromJson(Map<String, dynamic> json)
+      : x = json['x'],
+        y = json['y'];
+
+  Map<String, dynamic> toJson() {
+    return {
+      'x': x,
+      'y': y,
+    };
+  }
+}
