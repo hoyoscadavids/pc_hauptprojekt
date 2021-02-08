7a19c749fcf9b84f71e4cc270cb1bac9f931b5a2
Simon Hoyos Cadavid <45491683+hoyoscadavidsimon@users.noreply.github.com>
2021-02-08 20:31:12 +0100

    Corrected the distance calculator (x)

--- lib/utils/distance_calculator.dart
+++ lib/utils/distance_calculator.dart
@@ -6,9 +6,8 @@ import 'package:pc_hauptprojekt/location_detection/lat_lng.dart';
 class DistanceCalculator {
   /// Calculates the distance in x and y of two Coordinates in degrees
   static Vector calculate(LatLng start, LatLng end) {
-    final x = 6371000 * cos(end.latitude * pi / 180) * (end.longitude - start.longitude);
+    final x = 6371000 * pi / 180 * cos(end.latitude) * (end.longitude - start.longitude);
     final y = 111190 * (end.latitude - start.latitude);
-
     return Vector.column([x, y]);
   }
 }
